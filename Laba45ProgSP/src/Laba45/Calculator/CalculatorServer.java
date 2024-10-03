9e1e6f46e68499aeadf1f10f5b3119e17c94710b
KorolER <elizavetakorol2005@gmail.com>
2024-10-03 19:20:30 +0300

    проверка

--- Laba45ProgSP/src/Laba45/Calculator/CalculatorServer.java
+++ Laba45ProgSP/src/Laba45/Calculator/CalculatorServer.java
@@ -5,6 +5,8 @@ import java.net.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+// строчка для проверки гита
+
 public class CalculatorServer {
     
     public static void main(String[] args) {
